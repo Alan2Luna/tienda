0debd7d37d11c1470ef97f99a1ec791d5ef67ec7
Alan Luna <aklt1994@gmail.com>
2020-11-19 00:22:47 +0100

    header and cards

---
+++ index.js
@@ -0,0 +1,9 @@
+// const $button = document.querySelector('.header__toggleButton');
+// const $size = document.querySelector('.size__itemOption');
+
+// function toggleActive() {
+//     this.classList.toggle('isActive');
+// }
+
+// $button.addEventListener('click', toggleActive);
+// $size.addEventListener('click', toggleActive);
